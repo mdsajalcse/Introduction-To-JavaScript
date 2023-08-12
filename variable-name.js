df1642c5ef7e4fb4cc989471ebb65fac002b205d
sajal <mdsajalcse@gmail.com>
2023-08-12 22:01:45 +0600

    variable naming convention

---
+++ variable-name.js
@@ -0,0 +1,8 @@
+var userhomeaddress="Dhaka";  //snake case
+console.log(userhomeadddress);
+
+var userHomeAddress="chittagong"; //camelcase
+console.log(userHomeAddress);
+
+var UserHomeAddress="India"; //pascal case
+console.log(UserHomeAddress);
