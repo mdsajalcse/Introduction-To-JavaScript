f48df95d2e251677d7ee8a9b626daf1d2a40d2a6
sajal <mdsajalcse@gmail.com>
2023-08-12 21:31:15 +0600

    variable.js

---
+++ variable.js
@@ -0,0 +1,25 @@
+//numeric
+var price=11;
+var age=25;
+var temperature=37;
+console.log(price);
+console.log(age);
+console.log(temperature);
+
+
+// string
+
+var person= "Md. Sajal";
+console.log(person);
+var location= "Dhaka";
+console.log(location)
+var profession="Engineer";
+console.log(profession);
+
+//boolean
+var serious=true;
+console.log(serious);
+var isFullMarks=true;
+console.log(isFullMarks);
+var isBcsCadre=false;
+console.log(isBcsCadre);
